5235c749da57731e90c10e2be7b586f2e0296dec
Saiful Islam <codersaiful@gmail.com>
2018-12-14 23:48:12 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+include __DIR__ . '/testing.php';
+
+/**
+ * git@github.com:codersaiful/Gits.git ssh
+ * 
+ * 
+ * echo "# Gits" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:codersaiful/Gits.git
+git push -u origin master
+…or push an existing repository from the command line
+git remote add origin git@github.com:codersaiful/Gits.git
+git push -u origin master
+ */
