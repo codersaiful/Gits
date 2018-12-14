93bf50635d5c35b3754caeb9a48237e444a60495
Saiful Islam <codersaiful@gmail.com>
2018-12-14 23:49:25 +0600

    Testin Commit

--- index.php
+++ index.php
@@ -14,4 +14,15 @@ git push -u origin master
 …or push an existing repository from the command line
 git remote add origin git@github.com:codersaiful/Gits.git
 git push -u origin master
+
+
+echo "# Gits" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/codersaiful/Gits.git
+git push -u origin master
+…or push an existing repository from the command line
+git remote add origin https://github.com/codersaiful/Gits.git
+git push -u origin master
  */
